d4d37f336fe7a13ca99de4e253d9f6fcf9eb7a68
Manjushas123 <manjusha.s@compassitesinc.com>
2016-11-25 15:49:11 +0530

    Update view.php

--- Assessments/view.php
+++ Assessments/view.php
@@ -1,79 +1,51 @@
-
- <?php
-include 'database.php';
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-</head>
- 
-<body>
-    <div class="container">
-            <div class="row">
-                <h3>PHP CRUD </h3>
-            </div>
-            <div class="row">
-                <table class="table table-striped table-bordered">
-                <thead>
-                    <tr>
-                      <th>Employee Name</th>
-                      <th>Employee ID</th>
-                      <th>Designation</th>
-                      <th>Gender</th>
-                      <th>Experience</th>
-                      <th>Gross Salary</th>
-                      <th>Deduction</th>
-                      <th>LOP</th>
-                      <th>Net Salary</th>
-
-                    </tr>
-                  </thead>
-                
-                  <tbody><?php
-$pdo = Database::connect();
-$sql = 'SELECT * FROM employee_details ';
-foreach ($pdo->query($sql) as $row) { //print_r($row);exit;
-    echo '<tr>';
-    echo '<td>' . $row['empName'] . '</td>';
-    echo '<td>' . $row['empId'] . '</td>';
-    echo '<td>' . $row['designation'] . '</td>';
-    echo '<td>' . $row['gender'] . '</td>';
-    echo '<td>' . $row['experience'] . '</td>';
-    echo '<td>' . $row['gross_salary'] . '</td>';
-    echo '<td>' . $row['deduction'] . '</td>';
-    echo '<td>' . $row['lop'] . '</td>';
-    echo '<td>' . $row['netsal'] . '</td>';
-?>
-                           <td align="center"><?php
-    echo '<a href="update1.php?edit_id=' . $row["empid"] . '"><img src="edit.png" width="20" height="20"  align="EDIT" /></a>';
-?></td>
-        <td align="center"><a href="view.php?delete_id='<?php
-    echo $row[0];
-?>'"><img src="Remove.png"  width="20" height="20" align="DELETE" /></a>
-</td>
-        </tr>
-        <?php
-    echo '</tr>';
-}
-if (isset($_GET['delete_id'])) {
-    $pdo       = Database::connect();
-    $sql_query = "DELETE FROM employee_details WHERE empName=" . $_GET['delete_id'];
-    $pdo->query($sql_query);
-    header("Location: view.php");
-    /*if (mysqli_query($conn, $sql_query)) 
-    {
-    echo "hii";
+<html>
+<?php
+require "dbconfig.php";
+   // require 'db_select.php';
+    $sql1 = "SELECT * FROM employee_detail";
+    $result = $conn->query($sql1);
+    if ($result->num_rows > 0) {
+    ?>
+    <table>
+    <tr>
+    <th>Employee Name</th>
+    <th>Employee Id</th>
+    <th>Designation</th>
+    <th>Gender</th>
+    <th>Experience</th>
+    <th>Gross Salary</th>
+    <th>Deduction</th>
+    <th>LOP</th>
+    <th>Net Salary</th>
+    </tr>
+    <?php
+    while($row = $result->fetch_assoc()) {   
+        echo "<tr>
+        <td>".$row["empName"]."</td>
+        <td>".$row["empId"]."</td>
+        <td>".$row["designation"]."</td>
+        <td>".$row["gender"]."</td>
+        <td>".$row["experience"]."</td>
+        <td>".$row["gross_salary"]."</td>
+        <td>".$row["deduction"]."</td>
+        <td>".$row["lop"]."</td>
+        <td>".$row["netsal"]."</td>";
+        echo '<td width=250>';
+        //$calculate=$_POST['gross_salary']-($_POST['deduction']+ $_POST['lop']);
+         //echo $calculate;
+        echo '<a href="delete.php?id='. $row['id'] .'">Remove</a>';
+        echo ' ';
+        echo '<a href="update.php?id='. $row['id'] .'">Edit</a>';
+        echo "</tr>";
     }
-    else
-    {
-    echo "hbsjsn";
-    }*/
-}
+    ?>
+    <a href ="index.php"> Return to the home page </a>
+    </table>
+    <?php
+    } else {
+     echo "0 results";
+    }   
+mysqli_close($conn);
 ?>
-                 </tbody>
-            </table>
-        </div>
-    </div> <!-- /container -->
-  </body>
-</html>
+ 
+    
