41a2238afe328a5b6d16cc522b9831808850ee91
Manjushas123 <manjusha.s@compassitesinc.com>
2016-11-28 10:09:00 +0530

    Update update.php

--- Assessments/update.php
+++ Assessments/update.php
@@ -1,22 +1,7 @@
 <?php
-// including the database connection file
-require "calculate.php";
-$servername = "localhost";
-$username   = "root";
-$password   = "compass";
-$dbname     = "mytodo";
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    
-    die("Connection failed: " . mysqli_connect_error());
-}
-//getting id from url
+require "dbconfig.php";
 $id = $_GET['id'];
-
 if (isset($_POST['update'])) {
-    //global $mysqli;
     $empname     = $_POST['empName'];
     $empid       = $_POST['empId'];
     $designation = $_POST['designation'];
@@ -25,27 +10,22 @@ if (isset($_POST['update'])) {
     $grosssalary = $_POST['gross_salary'];
     $deduction   = $_POST['deduction'];
     $lop         = $_POST['lop'];
-    $netsalary   = $_POST['netsal'];
-    // checking empty fields
+    $netsalary   = $_POST['gross_salary']-((($_POST['gross_salary']/30) *$_POST['lop'])+$_POST['deduction']);
     
-    //updating the table
-    $result = "UPDATE employee_detail SET empName ='$empname',empId='$empid',designation='$designation',gender='$gender',experience='$experience',gross_salary='grosssalary',deduction='$deduction',lop='$lop',netsal='$netsalary' WHERE id=$id";
-    //redirectig to the display page. In our case, it is index.php
-    //  header("Location: index.php");
-    $re     = $conn->query($result);
-    if ($re) {
-        
+    $result = "UPDATE employee_detail SET empName ='$empname', empId=$empid,designation='$designation',gender='$gender',experience=$experience,gross_salary=$grosssalary,deduction=$deduction,lop=$lop,netsal=$netsalary WHERE id=$id";
+    $result_query     = $conn->query($result);
+    if ($result_query) {
+        echo "<i>";
+        echo "<p align = center>";
         echo "Record updated Successfully";
+        echo "</p>";
+        echo "</i>";
     }
 }
-
 ?>
-<a href = "view1.php"> Return to the main page </a>
+<p color = "blue"><b><a href = "view.php"> Go to the View Page</a></b></p>
 <?php
-
-//selecting data associated with this particular id
 $result = mysqli_query($conn, "SELECT * FROM employee_detail WHERE id=$id");
-
 $res         = mysqli_fetch_array($result);
 $empname     = $res['empName'];
 $empid       = $res['empId'];
@@ -57,17 +37,14 @@ $deduction   = $res['deduction'];
 $lop         = $res['lop'];
 $netsalary   = $res['netsal'];
 ?>
-
 <html>
 <head>
 <title> Employee Details </title>
-</head<td><?php
-echo $row[1];
-?></td></head>
-<body bgcolor = "pink">
+</head>
+<body bgcolor = #CCFFFF>
 <form method = "post" action= "">
 <table>
-<h1 align = "center"> Employee CRUD Application </h1>
+<h1 align = "center"> Employee Update operation</h1>
 <tr> <th> Employee Name </th> <td> <input id ="text" name= "empName" value="<?php
 echo $empname;
 ?>" required/> </td></tr>
@@ -93,9 +70,8 @@ echo $lop;
 ?>" required/></td></tr>
   <tr> <th> Net Salary </th> <td><input id ="number" name ="netsal" value="<?php
 echo $netsalary;
-?>" required/></td></tr>
-<tr> <td><input type="submit" name="update" value="update" /></td></tr>
-  
+?>" /></td></tr>
+<tr> <td><p align= "center"><input type="submit" name="update" value="update" /></p></td></tr> 
   </table>
   </form>
   <p align="center"></p>
