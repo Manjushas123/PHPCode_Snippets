c1490eceadef32b8d49d919d03bc62361f97e8c0
Manjusha S <manjusha.s@compassitesinc.com>
2016-12-20 16:20:36 +0530

    delete operation on slave

--- Assessments/dbOperation.php
+++ Assessments/dbOperation.php
@@ -13,12 +13,26 @@ class DBOperations
         $this->conn = mysqli_connect($this->db_hostname, $this->db_username, $this->db_password, $this->db_name);
         if (!$this->conn) {
             die("Unable to connect database" . mysqli_error($this->conn));
-        }   
+        }
     }
     public function Delete($id)
     {
-        $delete_query = "DELETE employee_detail ,salary_details from employee_detail e INNER JOIN salary_details el on e.id=el.id where e.id = {$id}";
-        $result       = $this->conn->query($delete_query);
+        $delete_query = "delete from employee_detail where id= {$id}";
+        echo $delete_query;
+        
+        $result = $this->conn->query($delete_query);
+        if ($result === true) {
+            return true;
+        } else {
+            return mysqli_error($this->conn);
+        }
+    }
+    public function DeleteSalary($id)
+    {
+        $delete_query = "DELETE from salary_details where id= {$id}";
+        echo $delete_query;
+        
+        $result = $this->conn->query($delete_query);
         if ($result === true) {
             return true;
         } else {
@@ -28,9 +42,50 @@ class DBOperations
     public function ReadRecord($id)
     {
         $read_query = "SELECT * from employee_detail e LEFT OUTER JOIN salary_details el on e.id = el.id where e.id = {$id}";
-        $result = $this->conn->query($read_query);
+        $result     = $this->conn->query($read_query);
         $row = $result->fetch_assoc();
-        return $row;    
+        return $row;
+    }
+    public function ReadRecordByRow($id)
+    {
+        $read_query = "SELECT * from employee_detail e LEFT OUTER JOIN salary_details el on e.id = el.id where e.id = {$id}";
+        $result     = $this->conn->query($read_query);
+        ?>
+        <a href = "index.php">Back </a>
+        <h3> Salary Details </h3>
+        <table border =5px>
+
+        <?php
+        foreach ($result as $value) {
+            
+            echo "<tr>";
+            echo "<th> Salary </th>";
+            echo "<td>";
+            echo $value['salary'];
+            echo "</td>";
+            echo "</tr>";
+            echo "<tr>";
+            echo "<th>Day </th>";
+            echo "<td>";
+            echo $value['day'];
+            echo "</td>";
+            echo "</tr>";
+            echo "<tr>";
+            echo "<th>Month </th>";
+            echo "<td>";
+            echo $value['month'];
+            echo "</td>";
+            echo "</tr>";
+            echo "<tr>";
+            echo "<th>Year</th>";
+            echo "<td>";
+            echo $value['year'];
+            echo "</td>";
+            echo "</tr>";
+        }
+    ?>
+    </table>
+    <?php
     }
     public function ListStudents()
     {
@@ -38,7 +93,7 @@ class DBOperations
         $resultset  = $this->conn->query($view_query);
         return $resultset;
     }
-     public function ListSalary()
+    public function ListSalary()
     {
         $view_query = "SELECT * FROM salary_details";
         $resultset  = $this->conn->query($view_query);
@@ -46,17 +101,17 @@ class DBOperations
     }
     public function createRecord($data)
     {
-        $empName = $data['empName'];
-        $empId = $data['empId'];
-        $designation = $data['designation'];
-        $gender = $data['gender'];
-        $experience = $data['experience'];
-        $grossSalary = $data['gross_salary'];
-        $deduction = $data['deduction'];
-        $lop = $data['lop'];
+        $empName      = $data['empName'];
+        $empId        = $data['empId'];
+        $designation  = $data['designation'];
+        $gender       = $data['gender'];
+        $experience   = $data['experience'];
+        $grossSalary  = $data['gross_salary'];
+        $deduction    = $data['deduction'];
+        $lop          = $data['lop'];
         $errorMessage = "";
-        $calculation = new calculate();
-        $netSalary = $calculation->calculation($data);
+        $calculation  = new calculate();
+        $netSalary    = $calculation->calculation($data);
         $insert_query = "INSERT INTO employee_detail(empName,empId,designation,gender,experience,gross_salary,deduction,lop,netsal) VALUES('$empName',$empId,
         '$designation','$gender',$experience,$grossSalary,$deduction,$lop,$netSalary)";
         
@@ -64,47 +119,47 @@ class DBOperations
             return true;
         } else if (!mysqli_query($this->conn, $insert_query)) {
             return false;
-        }    
+        }
     }
     public function update($id)
     {
-        $empName = $_POST['empName'];
-        $empId = $_POST['empId'];
-        $designation = $_POST['designation'];
-        $gender = $_POST['gender'];
-        $experience = $_POST['experience'];
-        $grossSalary = $_POST['gross_salary'];
-        $deduction = $_POST['deduction'];
-        $lop = $_POST['lop'];
+        $empName      = $_POST['empName'];
+        $empId        = $_POST['empId'];
+        $designation  = $_POST['designation'];
+        $gender       = $_POST['gender'];
+        $experience   = $_POST['experience'];
+        $grossSalary  = $_POST['gross_salary'];
+        $deduction    = $_POST['deduction'];
+        $lop          = $_POST['lop'];
         $errorMessage = "";
-        $calculation = new calculate();
-        $netSalary = $calculation->calculation($_POST);
+        $calculation  = new calculate();
+        $netSalary    = $calculation->calculation($_POST);
         $result_query = "UPDATE  employee_detail SET empName = '$empName', empId = $empId, designation = '$designation',gender = '$gender',experience = $experience,gross_salary = $grossSalary,deduction = $deduction,lop = $lop,netsal = $netSalary WHERE id = $id";
-        $result  = $this->conn->query($result_query);
+        $result       = $this->conn->query($result_query);
         if ($result) {
-            return true; 
+            return true;
         } else {
             return false;
-        }   
+        }
     }
     public function createSalaryRecord($data)
     {
         //echo "hii";
-        $id = $data['id'];
-        $empName = $data['empName'];
-        $salary = $data['salary'];
-        $day = $data['day'];
-        $month = $data['month'];
-        $year = $data['year'];
+        $id          = $data['id'];
+        $empName     = $data['empName'];
+        $salary      = $data['salary'];
+        $day         = $data['day'];
+        $month       = $data['month'];
+        $year        = $data['year'];
         $insertQuery = "INSERT INTO salary_details(id,empName,salary,day,month,year) VALUES($id,'$empName',$salary,
         $day,$month,$year)";
         //print($insertQuery);
-        $result  = $this->conn->query($insertQuery);
+        $result      = $this->conn->query($insertQuery);
         if ($result) {
-            return true; 
+            return true;
         } else {
             return false;
-        }          
+        }
     }
 }
 ?> 
