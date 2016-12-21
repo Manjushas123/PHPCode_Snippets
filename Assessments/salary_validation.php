6881015e553e5bb086b8736c988d297c63b2c828
Manjusha S <manjusha.s@compassitesinc.com>
2016-12-21 15:05:15 +0530

    salary validation

---
+++ Assessments/salary_validation.php
@@ -0,0 +1,39 @@
+<?php
+Class validate {
+function validation($data)
+{
+    $empName = $data['empName'];
+    $salary =$data['salary'];
+    $day =$data['day'];
+    $month =$data['month'];
+    $year =$data['year'];
+    $valid  = true;
+    $errMsgs = array();
+    if (empty($salary) || !preg_match("/^0$|^[-]?[1-9][0-9]*$/", $salary) ) {
+        $salaryError = "Please enter the valid Salary";
+        $errMsgs['salary'] = $salaryError;
+        $valid = false;
+    }
+    if (empty($day) || !preg_match("/^0$|^[-]?[1-9][0-9]*$/", $day) || $day >31) {
+        $dayError = "Please enter the valid day";
+        $errMsgs['day'] = $dayError;
+        $valid = false;
+    }
+   
+    if (empty($month) || !preg_match("/^0$|^[-]?[1-9][0-9]*$/", $month)) {
+        $monthError = "Please enter the valid month";
+        $errMsgs['month'] = $monthError;
+        $valid =false;
+        
+    }
+    if (empty($year) || !preg_match("/^0$|^[-]?[1-9][0-9]*$/", $year) ) {
+        $yearError = "Please enter the valid year";
+        $errMsgs['year'] = $yearError;
+        $valid = false;
+    }
+    
+    $resp = array("status" => $valid,"message" => $errMsgs);
+    return $resp;   
+}
+}
+?> 
